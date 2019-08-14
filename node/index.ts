c0de80d7df6e9825069f7fe042c9ab21c94f45b6
Enzo Nakamura <enzo.nakamura@vtex.com.br>
2019-08-14 20:13:57 -0300

    new version

--- node/index.ts
+++ node/index.ts
@@ -1,17 +1,24 @@
 import { Service } from '@vtex/api'
 
-const listaDeUsuarios = [
-    {photo: "https://avatars3.githubusercontent.com/u/3827456?s=460&v=4", id: "1", nome: "Artur", friends: [1,3,4], ano: 1999}, 
-    {photo: "https://avatars1.githubusercontent.com/u/8623116?s=460&v=4", id: "2", nome:"Anita", friends: [], ano: 1997}, 
-    {photo: "https://avatars0.githubusercontent.com/u/7076941?s=460&v=4", id: "3", nome: "Enzo", friends: [], ano: 1995}, 
-    {photo: "https://avatars1.githubusercontent.com/u/36630479?s=400&v=4", id: "4", nome: "Laercio", friends: [2,3], ano: 1996}, 
-    {photo: "https://avatars2.githubusercontent.com/u/34658092?s=400&v=4", id: "5", nome: "Davi", friends: [1,4], ano: 1998},
-    {photo: "https://avatars3.githubusercontent.com/u/3827456?s=460&v=4", id: "6", nome: "Artur", friends: [1,3,4], ano: 1999}, 
-    {photo: "https://avatars1.githubusercontent.com/u/8623116?s=460&v=4", id: "7", nome:"Anita", friends: [], ano: 1997}, 
-    {photo: "https://avatars0.githubusercontent.com/u/7076941?s=460&v=4", id: "8", nome: "Enzo", friends: [], ano: 1995}, 
-    {photo: "https://avatars1.githubusercontent.com/u/36630479?s=400&v=4", id: "9", nome: "Laercio", friends: [2,3], ano: 1996}, 
-    {photo: "https://avatars2.githubusercontent.com/u/34658092?s=400&v=4", id: "10", nome: "Davi", friends: [1,4], ano: 1998}
+var listaDeUsuarios = [
+    {photo: "https://avatars3.githubusercontent.com/u/3827456?s=460&v=4", id: "0", nome: "Artur", friends: [1,3,4], ano: 1999}, 
+    {photo: "https://avatars1.githubusercontent.com/u/8623116?s=460&v=4", id: "1", nome:"Anita", friends: [], ano: 1997}, 
+    {photo: "https://avatars0.githubusercontent.com/u/7076941?s=460&v=4", id: "2", nome: "Enzo", friends: [], ano: 1995}, 
+    {photo: "https://avatars1.githubusercontent.com/u/36630479?s=400&v=4", id: "3", nome: "Laercio", friends: [2,3], ano: 1996}, 
+    {photo: "https://avatars2.githubusercontent.com/u/34658092?s=400&v=4", id: "4", nome: "Davi", friends: [1,4], ano: 1998},
+    {photo: "https://avatars3.githubusercontent.com/u/3827456?s=460&v=4", id: "5", nome: "Artur", friends: [1,3,4], ano: 1999}, 
+    {photo: "https://avatars1.githubusercontent.com/u/8623116?s=460&v=4", id: "6", nome:"Anita", friends: [], ano: 1997}, 
+    {photo: "https://avatars0.githubusercontent.com/u/7076941?s=460&v=4", id: "7", nome: "Enzo", friends: [], ano: 1995}, 
+    {photo: "https://avatars1.githubusercontent.com/u/36630479?s=400&v=4", id: "8", nome: "Laercio", friends: [2,3], ano: 1996}, 
+    {photo: "https://avatars2.githubusercontent.com/u/34658092?s=400&v=4", id: "9", nome: "Davi", friends: [1,4], ano: 1998}
 ]
+
+const sortASC = (a: any, b: any) => {
+    return a.friends.length < b.friends.length ? -1 : a.friends.length > b.friends.length ? 1 : 0
+}
+const sortDESC = (a: any, b: any) => {
+    return a.friends.length < b.friends.length ? 1 : a.friends.length > b.friends.length ? -1 : 0
+}
 
 export default new Service({
     graphql: {
@@ -19,7 +26,14 @@ export default new Service({
             Query: {
                 usuario: (_, args) => (search(args.id)),
                 usuarios: (_, args) => {
-                    const {from, to} = args
+                    const {from, to, ord} = args
+
+                    if (ord === "ASC") {
+                        listaDeUsuarios.sort(sortASC)
+                    } else if (ord === "DESC") {
+                        listaDeUsuarios.sort(sortDESC)
+                    }
+                    
                     return listaDeUsuarios.slice(from, to)
                 },
             },
@@ -34,6 +48,12 @@ export default new Service({
                     return {
                         usuario: search(index.toString())
                     }
+                },
+                alteraIdade: (_, args) => {
+                    console.log(listaDeUsuarios[parseInt(args.id)].ano)
+                    listaDeUsuarios[parseInt(args.id)].ano--
+                    console.log(listaDeUsuarios[parseInt(args.id)].ano)
+                    return listaDeUsuarios[parseInt(args.id)].ano
                 }
             }
         }
--- react/MyApp.tsx
+++ react/MyApp.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useEffect, useState, ReactText } from "react";
-import { Table } from "vtex.styleguide";
-import { Query } from "react-apollo";
-import pessoas from "./graphql/queries/pessoas.gql";
+import React, { FC, useEffect, useState } from "react"
+import { Table, Box } from "vtex.styleguide"
+import { Query } from "react-apollo"
+import { useRuntime } from 'vtex.render-runtime'
+import pessoas from "./graphql/queries/pessoas.gql"
 
 interface PessoasData {
   usuarios: {
+    id: string,
     photo: string;
     nome: string;
     amigos: {
@@ -21,76 +23,132 @@ const MyApp: FC = () => {
     window.postMessage({ action: { type: "STOP_LOADING" } }, "*");
   });
 
-  const [tableLen, setTableLen] = useState(5)
-  const [from, setFrom] = useState(0)
-  const [to, setTo] = useState(tableLen)
+  const [tableLength, setTableLength] = useState(5);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentItemFrom, setCurrentItemFrom] = useState(0);
+  const [currentItemTo, setCurrentItemTo] = useState(tableLength);
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortedBy, setSortedBy] = useState(null);
+
+  const handleNextClick = () => {
+    const newPage = currentPage + 1;
+    const itemFrom = currentItemTo;
+    const itemTo = tableLength * (newPage + 1);
+
+    goToPage(newPage, itemFrom, itemTo);
+  };
+
+  const handlePrevClick = () => {
+    if (currentPage === 0) return;
+
+    const newPage = currentPage - 1;
+    const itemFrom = currentItemFrom - tableLength;
+    const itemTo = currentItemFrom;
+
+    goToPage(newPage, itemFrom, itemTo);
+  };
+
+  const goToPage = (
+    currentPage: any,
+    currentItemFrom: any,
+    currentItemTo: any
+  ) => {
+    setCurrentPage(currentPage);
+    setCurrentItemTo(currentItemTo);
+    setCurrentItemFrom(currentItemFrom);
+  };
+
+  const handleRowsChange = (e: any, value: string | number) => {
+    const v = typeof value == "string" ? parseInt(value) : value;
+    const start = currentItemFrom;
+
+    setTableLength(v);
+    setCurrentPage(Math.floor(start / v));
+    setCurrentItemFrom(v * Math.floor(start / v));
+    setCurrentItemTo(v * Math.floor(start / v) + v);
+  };
+
+  const { navigate } = useRuntime();
 
   return (
     <div>
-      <PessoasQuery query={pessoas} variables={{from, to}}>
+      <PessoasQuery
+        query={pessoas}
+        variables={{ from: currentItemFrom, to: currentItemTo, ord: sortOrder }}
+      >
         {({ data }) => {
+          console.log(data)
           if (data && data.usuarios) {
-            const info = data.usuarios.map(x => {
-              if (x.nome === "Anita") {
-                return {
-                  photo: x.photo,
-                  nome: x.nome,
-                  numeroDeAmigos: -20,
-                  idade: x.idade
-                };
-              } else {
-                return {
-                  photo: x.photo,
-                  nome: x.nome,
-                  numeroDeAmigos: x.amigos.length,
-                  idade: x.idade
-                };
-              }
+            console.log(data.usuarios)
+            var info = data.usuarios.map(x => {
+              return {
+                id: x.id,
+                photo: x.photo,
+                nome: x.nome,
+                numeroDeAmigos: x.amigos.length,
+                idade: x.idade
+              };
             });
 
-            console.log(info)
             return (
-              <Table
-                density={'low'}
-                schema={{
-                  properties: {
-                    photo: {
-                      cellRenderer: ({ cellData } : any) => (
-                        <img src = {cellData} style={{height: 70, width: 70}}/>
-                      ),
-                      headerRenderer: (): null => null,
-                      width: 100
-                    },
-                    nome: {
-                      title: "Nome"
-                    },
-                    numeroDeAmigos: {
-                      title: "Número de amigos"
-                    },
-                    idade: {
-                      title: "Idade"
-                    }
-                  }
-                }}
-                items={info}
-                pagination={{
-                  onNextClick: () => {},
-                  onPrevClick: () => {},
-                  currentItemFrom: from,
-                  currentItemTo: to,
-                  onRowsChange: (e, value: number | string) => {
-                    setFrom(0)
-                    const v = typeof value == "string" ? parseInt(value) : value
-                    setTableLen(v)
-                    setTo(v)
-                  },
-                  textShowRows: 'Show rows',
-                  textOf: 'of',
-                  totalItems: 0,
-                  rowsOptions: [1,2,3,4,5,6,7,8,9,10],
-                  selectedOption: tableLen
-                }}
-              />
+              <div className={"mw7"}>
+                <Box>
+                  <Table
+                    density={"low"}
+                    schema={{
+                      properties: {
+                        photo: {
+                          cellRenderer: ({ cellData }: any) => (
+                            <img
+                              src={cellData}
+                              style={{ height: 70, width: 70 }}
+                            />
+                          ),
+                          headerRenderer: (): null => null,
+                          width: 100
+                        },
+                        nome: {
+                          title: "Nome"
+                        },
+                        numeroDeAmigos: {
+                          title: "Número de amigos",
+                          sortable: true
+                        },
+                        idade: {
+                          title: "Idade"
+                        }
+                      }
+                    }}
+                    items={info}
+                    pagination={{
+                      onNextClick: handleNextClick,
+                      onPrevClick: handlePrevClick,
+                      currentItemFrom: currentItemFrom + 1,
+                      currentItemTo: currentItemTo,
+                      onRowsChange: handleRowsChange,
+                      textShowRows: "Show rows",
+                      textOf: "of",
+                      totalItems: 10,
+                      rowsOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                      selectedOption: tableLength
+                    }}
+                    sort={{
+                      sortedBy: sortedBy,
+                      sortOrder: sortOrder
+                    }}
+                    onSort={({sortedBy, sortOrder}: {sortedBy: any, sortOrder: any}) => {
+                      setSortOrder(sortOrder)
+                      setSortedBy(sortedBy)
+                    }}
+                    onRowClick={({ rowData }: { rowData: any }) => {
+                      navigate({
+                        page: "nomedoapp.outrapag",
+                        params: { userId: rowData.id }
+                      })
+                    }}
+                  />
+                </Box>
+              </div>
             );
           } else {
             return <div></div>;
